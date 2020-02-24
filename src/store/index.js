70c02b3d88803207d601511bf41f495a8dd20459
tofu511 <mokmok511@gmail.com>
2020-02-24 16:43:47 +0900

    Refactor: persist data

--- src/store/index.js
+++ src/store/index.js
@@ -29,6 +29,8 @@ const store = {
       const todos = localStorage.getItem('my_todos');
       if (!todos) {
         Vue.set(state, 'todos', INITIAL_DATA.todos);
+      } else {
+        Vue.set(state, 'todos', JSON.parse(todos));
       }
       return state.todos;
     },
@@ -36,23 +38,34 @@ const store = {
       // eslint-disable-next-line no-param-reassign
       todo.id = Math.random().toString(36).substr(2, 7);
       state.todos.push(todo);
+      return state.todos;
     },
     updateTodo(state, todo) {
       const index = state.todos.findIndex(t => t.id === todo.id);
       Vue.set(state.todos, index, todo);
+      return state.todos;
     },
     deleteTodo(state, id) {
       const index = state.todos.findIndex(t => t.id === id);
       state.todos.splice(index, 1);
+      return state.todos;
     },
   },
+};
+
+const persistData = (value) => {
+  localStorage.setItem('my_todos', JSON.stringify(value));
 };
 
 store.dispatch = (action, payload) => {
   if (!store.actions[action]) {
     throw new Error(`Action ${action} is not defined in the store`);
   }
-  return store.actions[action](store.state, payload);
+  const result = store.actions[action](store.state, payload);
+  if (result) {
+    persistData(result);
+  }
+  return result;
 };
 
 export default store;
